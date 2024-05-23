f01aa2f234b5619ebb76b032a92a861e49442bc8
umakarthik08 <123018877+umakarthik08@users.noreply.github.com>
2024-05-23 15:04:08 +0530

    About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,23 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a student at National Institute of Technology Raipur aspiring to see myself as a Software Developer";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Bootstrap",
+  "PHP",
+  "MYSQL",
+  "NodeJs",
+  "ReactJs",
+  "ExpressJs",
+  "MongoDB",
 ];
 
 /**
@@ -45,8 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "I am passionate and enthusiastic in learning new technologies and also competitive programmer with good stats in sites like CodeChef, Codeforces, Leetcode etc";
 const About = () => {
   return (
     <section className="padding" id="about">
